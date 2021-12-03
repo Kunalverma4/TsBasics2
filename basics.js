33b8e60e63bb807e44e10172c5e7fd2e1e606b14
kunalverma0412 <kverma829@gmail.com>
2021-12-03 11:50:01 +0530

    s

---
+++ basics.js
@@ -0,0 +1,10 @@
+// function log(message){
+//     console.log(message);
+// }
+// var message = 'hello'
+// log(message);
+var drawPoint = function (point) { };
+drawPoint({
+    x: 1,
+    y: 2
+});
---
+++ basics.ts
@@ -0,0 +1,27 @@
+// function log(message){
+//     console.log(message);
+// }
+// var message = 'hello'
+// log(message);
+
+
+// let message;
+// message = 'abc'
+// let endsWC = (<string>message).endsWith('c');
+// let alternate = (message as string).endsWith('c');
+
+// let log = function(message){
+//     console.log(message);
+    
+// }
+// let doLog = (message)=> console.log(message);
+
+interface Point{
+    x: number,
+    y:number
+}
+let drawPoint =(point : Point)=>{}
+drawPoint ({
+    x:1,
+    y:2
+})
