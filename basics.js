7958291f5c9ee017e94f552ffa48aeda2ea5b40f
kunalverma0412 <kverma829@gmail.com>
2021-12-03 11:53:02 +0530

    e

--- basics.js
+++ basics.js
@@ -1,8 +1,66 @@
-// function log(message){
-//     console.log(message);
-// }
-// var message = 'hello'
-// log(message);
+function log(message) {
+    console.log(message);
+}
+var message = 'hello';
+log(message);
+var message;
+message = 'abc';
+var endsWC = message.endsWith('c');
+var alternate = message.endsWith('c');
+var log = function (message) {
+    console.log(message);
+};
+var doLog = function (message) { return console.log(message); };
+var color;
+(function (color) {
+    color[color["red"] = 5] = "red";
+    color[color["green"] = 6] = "green";
+    color[color["blue"] = 7] = "blue";
+})(color || (color = {}));
+;
+var c = color.blue;
+console.log(c);
+var randomvalue = 10;
+randomvalue = true;
+randomvalue = 'kunal';
+var myVariable = 10;
+function hasName(obj) {
+    return !!obj &&
+        typeof obj === "object" &&
+        'name' in obj;
+}
+if (hasName(myVariable)) {
+    console.log(myVariable.name);
+}
+var a;
+a = 10;
+a = true;
+var b = 20;
+var multiType;
+multiType = 20;
+multiType = true;
+var anyType;
+anyType = 20;
+anyType = true;
+function add(num1, num2) {
+    if (num2 === void 0) { num2 = 10; }
+    if (num2) {
+        return num1 + num2;
+    }
+    else {
+        return num1;
+    }
+}
+var A = add(2);
+console.log(A); // answer 12
+function fullName(person) {
+    console.log("".concat(person.firstName, " ").concat(person.lastName));
+}
+var i = {
+    firstName: 'kunal',
+    lastName: 'verma'
+};
+fullName(i);
 var drawPoint = function (point) { };
 drawPoint({
     x: 1,
--- basics.ts
+++ basics.ts
@@ -1,20 +1,78 @@
-// function log(message){
-//     console.log(message);
-// }
-// var message = 'hello'
-// log(message);
+function log(message){
+    console.log(message);
+}
+var message = 'hello'
+log(message);
 
 
-// let message;
-// message = 'abc'
-// let endsWC = (<string>message).endsWith('c');
-// let alternate = (message as string).endsWith('c');
+let message;
+message = 'abc'
+let endsWC = (<string>message).endsWith('c');
+let alternate = (message as string).endsWith('c');
 
-// let log = function(message){
-//     console.log(message);
+let log = function(message){
+    console.log(message);
     
-// }
-// let doLog = (message)=> console.log(message);
+}
+let doLog = (message)=> console.log(message);
+enum color {red=5,green,blue};
+let c:color = color.blue
+console.log(c);
+
+let randomvalue :any = 10;
+randomvalue = true;
+randomvalue= 'kunal'
+
+let myVariable:unknown = 10;
+function hasName(obj:any): obj is{name:string}{
+    return !!obj &&
+    typeof obj === "object" &&
+    'name' in obj
+}
+if (hasName(myVariable)){
+console.log(myVariable.name);
+
+}
+
+let a;
+a=10;
+a=true;
+
+let b = 20;
+
+let multiType: number | boolean;
+multiType = 20;
+multiType = true;
+
+let anyType:any;
+anyType = 20;
+anyType = true;
+
+function add(num1:number,num2:number =10):number {
+    if(num2){
+        return num1+num2;
+    }
+    else{
+        return num1;
+    }
+}
+
+let A=add(2)
+console.log(A); // answer 12
+
+interface Person {
+    firstName : string;
+    lastName : string;
+}
+function fullName(person: Person){
+    console.log(`${person.firstName} ${person.lastName}`);   
+}
+let i = {
+    firstName :'kunal',
+    lastName :'verma'
+}
+fullName(i);
+
 
 interface Point{
     x: number,
